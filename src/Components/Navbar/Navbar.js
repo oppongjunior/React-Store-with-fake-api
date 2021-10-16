dd29c2b49116c70ff3ee5a841c699d7b92a4d11e
Sean14031998 <41885942+Sean14031998@users.noreply.github.com>
2021-10-16 21:48:56 +0000

    almost done

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -20,7 +20,7 @@ function Navbar() {
         <>
             <nav className="navbar navbar-expand-lg navbar-dark bg-dark shadow">
                 <div className="container">
-                    <Link className="navbar-brand" to="/">Navbar</Link>
+                    <Link className="navbar-brand text-uppercase" to="/">Store App</Link>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
